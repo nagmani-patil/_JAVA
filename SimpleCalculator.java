1d45baf721abe3de27698d4bba608ba77f8a1539
@Nagmanicodes <patilnagmani1@gmail.com>
2025-11-16 10:54:50 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,15 @@
+//Make a Java program that takes 2 numbers and prints sum, product, and difference.
+import java.util.Scanner;
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+    }
+}
